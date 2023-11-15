590033ac24c448ee263c3bd5cfd04f2bb73b5089
amcerbu <amcerbu@gmail.com>
2023-11-15 01:13:08 -0800

    added forward-FFT-only 'Analysis' class

--- fourier.h
+++ fourier.h
@@ -132,6 +132,91 @@ namespace soundmath
 
 		int current = 0;
 	};
+
+
+	template <typename T, size_t N> class Analyzer
+	{
+	public:
+		int (*processor)(const T* in);
+
+		// in, middle, out need to be arrays of size (N * laps * 2)
+		Analyzer(int (*processor)(const T*), ShyFFT<T, N, RotationPhasor>* fft, size_t laps, T* in, T* middle) 
+			: processor(processor), in(in), middle(middle), fft(fft), laps(laps), stride(N / laps)
+		{
+			writepoints = new int[laps];
+
+			memset(writepoints, 0, sizeof(int) * laps);
+
+			for (int i = 0; i < (int)laps; i++) // initialize half of writepoints
+				writepoints[i] = -i * (int)stride;
+
+			writing = new bool[laps];
+			memset(writing, true, sizeof(bool) * laps);
+		}
+
+		~Analyzer()
+		{
+			delete [] writepoints;
+			delete [] writing;
+		}
+
+		// writes a single sample (with windowing) into the in array
+		void write(T x)
+		{
+			for (size_t i = 0; i < laps; i++)
+			{
+				if (writing[i])
+				{
+					if (writepoints[i] >= 0)
+					{
+						// T window = halfhann((T)writepoints[i] / N);
+						T window = hann((T)writepoints[i] / N);
+						in[writepoints[i] + N * i] = window * x;
+					}
+					writepoints[i]++;
+
+					if (writepoints[i] == N)
+					{
+						writing[i] = false;
+
+						forward(i); // FTs ith in to ith middle buffer
+						process(i); // user-defined; ought to move info from ith middle to out buffer
+						current = i;
+
+						size_t next = (i + 1) % (laps);
+						writing[next] = true;
+						writepoints[next] = 0;
+					}
+				}
+			}
+		}
+
+		inline void forward(const size_t i)
+		{
+			fft->Direct((in + i * N), (middle + i * N)); // analysis
+			// arm_rfft_fast_f32(fft, in + i * N, middle + i * N, 0);
+		}
+
+		// executes user-defined callback
+		inline void process(const size_t i)
+		{
+			processor((middle + i * N));
+		}
+
+	private:
+		T *in, *middle;
+
+	public:
+		ShyFFT<T, N, RotationPhasor>* fft;
+
+		size_t laps;
+		size_t stride;
+
+		int* writepoints;
+		bool* writing;
+
+		int current = 0;
+	};
 }
 
 #define FOURIER
